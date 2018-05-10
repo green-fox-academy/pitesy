5957c132d4015a938efe77da07abf5b3ec67b92b
Tofei <pitesy@gmail.com>
2018-05-10 11:39:39 +0200

    feat(ifExample)

---
+++ home/user/greenfox/pitesy/week-01/day-4/src/ifExample.java
@@ -0,0 +1,22 @@
+public class ifExample {
+
+    public static void main(String[] args) {
+        int number = 3;
+
+        if (number % 2 == 0) {
+            System.out.println("Paros");
+        } else {
+            System.out.println("paratlan");
+        }
+
+        if (number == 1) {
+            System.out.println("Szuper");
+        } else if (number == 2){
+            System.out.println("Majdnem szuper!");
+        } else {
+            System.out.println("Nem szuper!");
+        }
+    }
+}
+
+
