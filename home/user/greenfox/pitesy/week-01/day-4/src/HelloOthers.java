5917764d1972c204a8fabb9a50b73a0a2b01854d
Tofei <pitesy@gmail.com>
2018-05-10 12:54:05 +0200

    HelloOthers added

---
+++ home/user/greenfox/pitesy/week-01/day-4/src/HelloOthers.java
@@ -0,0 +1,7 @@
+public class HelloOthers {
+    public static void main(String[] args) {
+        System.out.println("Hello, Döme!");
+        System.out.println("Hello, Icu!");
+        System.out.println("Hello, Boldi!");
+    }
+}
