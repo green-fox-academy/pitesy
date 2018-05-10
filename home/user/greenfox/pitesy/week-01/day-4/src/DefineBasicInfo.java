ea4c718f0f08999c939c3e9649abcf84188f1f51
Tofei <pitesy@gmail.com>
2018-05-10 14:00:28 +0200

    Added DefineBasicInfo and VariableMutation

---
+++ home/user/greenfox/pitesy/week-01/day-4/src/DefineBasicInfo.java
@@ -0,0 +1,8 @@
+public class DefineBasicInfo {
+    public static void main(String[] args) {
+        String myName = "Tófei István";
+        int myAge = 29;
+        double myHeight = 1.84;
+        boolean maritalStatus = false;
+    }
+}
