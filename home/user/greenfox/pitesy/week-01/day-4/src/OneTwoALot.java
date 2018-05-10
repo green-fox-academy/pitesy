b9dcca840a7e141b0b211c694df1386bdbfb6c86
Tofei <pitesy@gmail.com>
2018-05-10 16:36:38 +0200

    Added OneTwoALot

---
+++ home/user/greenfox/pitesy/week-01/day-4/src/OneTwoALot.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class OneTwoALot {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter a number:");
+
+        int userInput = scanner.nextInt();
+
+        if (userInput <= 0) {
+            System.out.println("Not enough");
+        } else if (userInput == 1){
+            System.out.println("One");
+        } else if (userInput == 2){
+            System.out.println("Two");
+        } else {
+            System.out.println("A lot");
+        }
+    }
+}
