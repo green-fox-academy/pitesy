d83c409e6c74fcbd358b777a41ed86c200d0ee7c
Tofei <pitesy@gmail.com>
2018-05-10 13:03:36 +0200

    Added IntroduceYourself

---
+++ home/user/greenfox/pitesy/week-01/day-4/src/IntroduceYourself.java
@@ -0,0 +1,7 @@
+public class IntroduceYourself {
+    public static void main(String[] args) {
+        System.out.println("Tófei István");
+        System.out.println("29");
+        System.out.println(1.84);
+    }
+}
