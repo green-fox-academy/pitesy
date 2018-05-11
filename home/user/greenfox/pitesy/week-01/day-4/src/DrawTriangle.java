d6029b20b2a120aab182c45010e41a721bc2efa5
Tofei <pitesy@gmail.com>
2018-05-11 10:36:46 +0200

    Added DrawTriangle

---
+++ home/user/greenfox/pitesy/week-01/day-4/src/DrawTriangle.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class DrawTriangle {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner (System.in);
+
+    System.out.println("Please enter a number:");
+
+    int userInput = scanner.nextInt();
+
+    for (int i = 1; i <= userInput; i++) {
+      for (int j = 1; j <= i; j++) {
+        System.out.print("*");
+      }
+      System.out.println(" ");
+    }
+  }
+}
+
