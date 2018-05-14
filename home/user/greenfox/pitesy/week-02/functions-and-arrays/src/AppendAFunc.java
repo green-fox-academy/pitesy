f799a8a69b3ff8bc330471e848cee3de8ffb714c
Tofei <pitesy@gmail.com>
2018-05-14 13:02:34 +0200

    Added AppendAFunc

---
+++ home/user/greenfox/pitesy/week-02/functions-and-arrays/src/AppendAFunc.java
@@ -0,0 +1,12 @@
+public class AppendAFunc {
+
+  public static void main(String[] args) {
+
+    String am = "kuty";
+    appendA(am);
+  }
+
+  public static void appendA(String addA) {
+    System.out.println(addA + "a");
+  }
+}
