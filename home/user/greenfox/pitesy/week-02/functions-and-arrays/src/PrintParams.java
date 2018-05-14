d42ca39a1894a7ae7b21e9bb8a5ce3621bcd01d2
Tofei <pitesy@gmail.com>
2018-05-14 15:31:00 +0200

    Added new files

--- home/user/greenfox/pitesy/week-02/functions-and-arrays/src/PrintParams.java
+++ home/user/greenfox/pitesy/week-02/functions-and-arrays/src/PrintParams.java
@@ -1,18 +1,16 @@
 public class PrintParams {
   public static void main(String[] args) {
 
-//  String string1 = "first";
-//  String string2 = "second";
-//  String string3 = "third";
-//  String string4 = "fourth";
-//
-//  printParams(string1, string2, string3, string4);
-//
-//  }
-//
-//  public static void printParams(String text) {
+    String[] array = {"first", "second", "third", "fourth"};
+
+    printParams(array);
 
   }
 
+  public static void printParams(String[] text) {
+    for (int i = 0; i < text.length; i++) {
+      System.out.print (text[i] + " ");
+    }
+  }
 }
 
