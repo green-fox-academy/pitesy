bfb1ade1900acb914ba289204f73bae5e3deba06
Tofei <pitesy@gmail.com>
2018-05-14 12:58:52 +0200

    Created Doubling and Greet

---
+++ home/user/greenfox/pitesy/week-02/functions-and-arrays/src/Doubling.java
@@ -0,0 +1,15 @@
+public class Doubling {
+
+  public static void main(String[] args) {
+
+    int baseNum = 123;
+    baseNum = doubling(baseNum);
+    System.out.println(baseNum);
+  }
+
+  public static int doubling(int number) {
+    return number * 2;
+  }
+
+
+}
