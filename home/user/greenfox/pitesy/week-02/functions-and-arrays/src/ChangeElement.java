e2b4a4d56ce22c5602ffe67e6bf755dc5f6da754
Tofei <pitesy@gmail.com>
2018-05-14 16:15:06 +0200

    Added function to ChangeElement

--- home/user/greenfox/pitesy/week-02/functions-and-arrays/src/ChangeElement.java
+++ home/user/greenfox/pitesy/week-02/functions-and-arrays/src/ChangeElement.java
@@ -1,8 +1,14 @@
 public class ChangeElement {
-  public static void main(String[] args) {
-    int[] s = {1, 2, 3, 8, 5, 6};
-    s[3] = 4;
 
-    System.out.println(s[3]);
+  public static void main(String[] args) {
+
+    int[] s = {1, 2, 3, 8, 5, 6};
+    swap(s);
+  }
+
+  public static int[] swap(int[] number) {
+    number[3] = 4;
+    System.out.println(number[3]);
+    return number;
   }
 }
